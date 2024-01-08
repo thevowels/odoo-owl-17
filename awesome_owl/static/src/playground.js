203dabb45d5dc21c71cb756ed51c766051c4cdda
thevowels <aungphyohtet1@gmail.com>
2024-01-09 01:01:34 +0630

    Extracting Counter Component owl 1.2

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,18 +1,6 @@
 /** @odoo-module **/
-
+import { Counter } from "./counter/counter"
 import { Component, useState } from "@odoo/owl";
-
-export class Counter extends Component{
-    static template = "awesome_owl.counter";
-
-    setup(){
-    this.state = useState({value: 0})
-    }
-
-    increment(){
-        this.state.value++;
-    }
-}
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
