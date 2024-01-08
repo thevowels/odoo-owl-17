d81464d5c0eb9b2d8fa9e6f2c79055b58f0880af
thevowels <aungphyohtet1@gmail.com>
2024-01-09 01:41:38 +0630

    Adding Props validation Owl 1.5

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -4,9 +4,12 @@ import { Component } from "@odoo/owl";
 export class Card extends Component{
     static template="awesome_owl.Card"
 
-    static defaultProps={
-        title:"Title",
-        content:"Content"
+    // static defaultProps={
+    //     title:"Title",
+    //     content:"Content"
+    // }
+    static props={
+        title:{type:String},
+        content:{type:String, optional:true}
     }
-
 }
