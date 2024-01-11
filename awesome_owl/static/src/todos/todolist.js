6e3dc1d3285d6dd08aae093b9dbb822f277c98c8
thevowels <aungphyohtet1@gmail.com>
2024-01-11 14:54:52 +0630

    Adding todo dynamically. Finish owl 1.9

--- awesome_owl/static/src/todos/todolist.js
+++ awesome_owl/static/src/todos/todolist.js
@@ -4,14 +4,22 @@ import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todoitem";
 export class TodoList extends Component{
     static template="awesome_owl.TodoList";
-
-    setup(){
-        this.todos = useState([
-            {id:1,description:"do something",isCompleted:false},
-            {id:2,description:"Do Nothing",isCompleted:true}])
-    }
     static components = {TodoItem}
 
+    setup(){
+        this.todos = useState([]);
+        this.nextId= 0;
+    }
 
+    addTodo(ev){
+        if(ev.keyCode == 13 && ev.target.value != ""){
+            this.todos.push({
+                id:this.nextId++,
+                description: ev.target.value,
+                isCompleted: false
+            })
+            ev.target.value = ""
+        }
+    }
 
 }
