162bd15c617f519c3a9f3c27ff5b88835f1251ab
thevowels <aungphyohtet1@gmail.com>
2024-01-28 00:33:41 +0630

    Adding a Layout

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,9 +2,21 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { ControlPanel } from "@web/search/control_panel/control_panel";
+import { Layout } from "@web/search/layout";
+
+
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+    setup(){
+        this.display= {
+            ControlPanel:{},
+        }
+    }
 }
+
+
+AwesomeDashboard.template = "awesome_dashboard.AwesomeDashboard";
+AwesomeDashboard.components = { Layout }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
